15552def0c7ebe81efef8292016e51bbd832459f
dan850 <dan06982@gmail.com>
2023-02-06 22:35:08 -0600

    version 1.0

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "you are great!"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .foregroundColor(.black)
+                .padding(30.0)
+          
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.orange)
+                .italic()
+                .padding()
+            
+            
+            
+            HStack {
+                Button("Press me") {
+
+                    messageString = "you are awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                
+                Button("Press Me Too") {
+
+                    messageString = "you are cbool!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+           
         }
-        .padding()
     }
 }
 
